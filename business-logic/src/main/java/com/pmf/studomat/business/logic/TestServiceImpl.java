387d6b9927c2e1e15311e4fad451836d86f9fa9f
Tilen Hedžet <tilen.hedzet@student.math.hr>
2021-06-18 12:36:18 +0200

    added docker image of db

--- business-logic/src/main/java/com/pmf/studomat/business/logic/TestServiceImpl.java
+++ business-logic/src/main/java/com/pmf/studomat/business/logic/TestServiceImpl.java
@@ -3,7 +3,7 @@ package com.pmf.studomat.business.logic;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TestServiceImpl implements TestService {
+class TestServiceImpl implements TestService {
 
     @Override
     public String greetings() {
